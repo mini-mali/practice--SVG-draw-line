35c1d0d9f9286ba0eec3e21b33d7c7314b8d138d
Frantisek Rapcak <frantisek.rapcak@gmail.com>
2020-12-17 23:24:33 +0100

    Change SVG line length using JS

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('JS works');
+const STEP = 40;
+
+let plus = document.querySelector('.plus'),
+    minus = document.querySelector('.minus'),
+    svg = document.querySelector('svg'),
+    line = svg.querySelector('line'),
+    x2 = line.getAttribute('x2');
+
+
+plus.onclick = (e) => {
+  x2 += STEP;
+  line.setAttribute('x2', x2);
+};
+minus.onclick = (e) => {
+  x2 -= STEP;
+  line.setAttribute('x2', x2);
+};
